1b3f068766330dc18faf10e1e7bc24bb70345318
JoshuaDESKTOP <jc.grant22@gmail.com>
2019-06-03 09:20:27 -0700

    Added page content

--- lonergram-reloaded/src/pages/About.js
+++ lonergram-reloaded/src/pages/About.js
@@ -2,6 +2,19 @@ import React, { Component } from 'react';
 
 export default class About extends Component {
   render() {
-    return "about";
+    return (
+      <div className="row">
+        <div className="column full">
+          <div className="padded-inner">
+            <h2>About Lonergram</h2>
+            <blockquote>Hell is other people</blockquote>
+            <p>-Sartre</p>
+            <p>
+              Love pictures? Hate people? Get Lonergram, the picture unsharing platform that nobody's talking about.
+            </p>
+          </div>
+        </div>
+      </div>
+    )
   }
 }
